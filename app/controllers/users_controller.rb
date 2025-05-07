90e5f790ec89a856518679d2d06af4ea8c4e3f4d
Ethan <ethanolivieri56@gmail.com>
2025-05-07 09:36:36 -0400

    Added styling to displaying stock information and moved information to the portfolio page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,14 @@
 class UsersController < ApplicationController
   def portfolio
     @user = current_user
+
+    if params[:stock].present?
+      symbol = params[:stock].upcase
+      service = FinnhubService.new
+      profile = service.company_profile(symbol)
+      quote = service.quote(symbol)
+
+      @stock = Stock.new(ticker: symbol, name: profile["name"], price: quote["c"])
+    end
   end
 end

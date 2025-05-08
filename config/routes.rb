a22dbc0f837c93feff0fd3531473adb7fcdac863
Ethan <ethanolivieri56@gmail.com>
2025-05-08 08:13:48 -0400

    Created many to many relationship between Users and Stocks

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :user_stocks
   get "up" => "rails/health#show", as: :rails_health_check
   root "users#portfolio"
   post "portfolio", to: "users#portfolio"

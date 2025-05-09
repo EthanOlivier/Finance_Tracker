d3d8b80c3f92f97154f294fee242bb39792c6fac
Ethan <ethanolivieri56@gmail.com>
2025-05-09 11:42:39 -0400

    Added first and last name to user model, added bootstrap icons

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,4 +17,9 @@ class User < ApplicationRecord
   def can_add_stock?(ticker)
     under_stock_limit? && !has_stock?(ticker)
   end
+
+  def full_name
+    return "#{first_name} #{last_name}" if first_name || last_name
+    "Anonymous"
+  end
 end

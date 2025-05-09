4633898c0d1213ff6a1e03527ccd54844f6cccf3
Ethan <ethanolivieri56@gmail.com>
2025-05-09 08:31:50 -0400

    Added restrictions to prevent user from adding stocks that are already in their portfolio and if there are too many stocks in their portfolio

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def has_stock?(ticker)
+    stocks.where(ticker: ticker).exists?
+  end
+
+  def under_stock_limit?
+    stocks.count < 5
+  end
+
+  def can_add_stock?(ticker)
+    under_stock_limit? && !has_stock?(ticker)
+  end
 end
